d705e31767f2fbcc68ab652ca5a78209d64b2563
purushothamnk <purushotha8nk@gmail.com>
2024-07-13 23:03:53 +0530

    new file:   .idea/.gitignore

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./Hackthon/src/main/resources/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
